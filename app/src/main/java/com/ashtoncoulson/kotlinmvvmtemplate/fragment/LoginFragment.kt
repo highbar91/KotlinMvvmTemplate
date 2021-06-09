083992c2d2b06590e52e662c647dc4c7eca33b84
Ashton Coulson <ashtoncoulson@Ashtons-MacBook-Pro.local>
2021-06-09 01:27:13 -0400

    login screen layout

--- app/src/main/java/com/ashtoncoulson/kotlinmvvmtemplate/fragment/LoginFragment.kt
+++ app/src/main/java/com/ashtoncoulson/kotlinmvvmtemplate/fragment/LoginFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.ashtoncoulson.kotlinmvvmtemplate.R
-import com.ashtoncoulson.kotlinmvvmtemplate.model.State
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -14,17 +13,8 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         super.onViewCreated(view, savedInstanceState)
         hideActionBar()
 
-        val state = State.WEST_VIRGINIA
-
-        textview_first.text = when (state) {
-            State.ALABAMA -> State.ALABAMA.name
-            State.COLORADO -> State.COLORADO.postalCode
-            State.WEST_VIRGINIA -> State.WEST_VIRGINIA.displayName
-            else -> "aint no state"
-        }
-
-        button_first.setOnClickListener {
-            findNavController().navigate(R.id.action_first_fragment_to_second_fragment)
+        loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_login_to_dashboard)
         }
     }
 }
